0c452051700f3e9fb1fd25d807b34af078096048
dvaa <93278631+Dava-033@users.noreply.github.com>
2024-01-13 16:54:33 +0700

    updated commit

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
+import dotenv from "dotenv"; // Tambahkan dotenv
+
+dotenv.config(); // Load environment variables
 
 const app = express();
 
 app.use('/res', (req, res) => {
-        res.send('Expresss Vercel App Response')
+    res.send('Express Vercel App Response')
 });
 
-// app.use(cors({
-//     origin: 'http://localhost:3001', // Sesuaikan dengan origin React Anda
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-// }));
+app.use(cors({
+    origin: process.env.REACT_APP_FRONTEND_URL, // Gunakan environment variable
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(UserRoute);
 
-const PORT = 3000;
-app.listen(PORT, ()=> 
-console.log("Server up and running"));
+const PORT = process.env.PORT || 3000; // Gunakan environment variable PORT jika tersedia
+app.listen(PORT, () => console.log("Server up and running"));
 
 export default app;
